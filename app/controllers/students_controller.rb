f45b75064ab7363130958dd23722b8e844d23a5c
shaaibu <shaaibusuleiman@gamil.com>
2023-08-17 20:09:35 +0100

    fix linter errors

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,47 +1,47 @@
 class StudentsController < ApplicationController
-    def index
-        @students = Student.all
-    end
+  def index
+    @students = Student.all
+  end
 
-    def new
-        @student = Student.new
-    end
+  def new
+    @student = Student.new
+  end
 
-    def create
-        @student = Student.new(student_params)
-        if @student.save
-            redirect_to students_path
-        else
-            render :new
-        end
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to students_path
+    else
+      render :new
     end
+  end
 
-    def show
-        @student = Student.find(params[:id])
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      redirect_to student_path(@student)
+    else
+      render :edit
     end
+  end
 
-    def edit
-        @student = Student.find(params[:id])
-    end
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+    redirect_to students_path
+  end
 
-    def update
-        @student = Student.find(params[:id])
-        if @student.update(student_params)
-            redirect_to student_path(@student)
-        else
-            render :edit
-        end
-    end
+  private
 
-    def destroy
-        @student = Student.find(params[:id])
-        @student.destroy
-        redirect_to students_path
-    end
-
-    private
-
-    def student_params
-        params.require(:student).permit(:first_name, :last_name, :email)
-    end
+  def student_params
+    params.require(:student).permit(:first_name, :last_name, :email)
+  end
 end
