d678c63cc1d9c12d0576a9265875f493ef48466d
shaaibu <shaaibusuleiman@gamil.com>
2023-08-18 09:52:28 +0100

    render partials and clean controller flow

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,6 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: %i[show edit update destroy]
+
   def index
     @students = Student.all
   end
@@ -17,15 +19,12 @@ class StudentsController < ApplicationController
   end
 
   def show
-    @student = Student.find(params[:id])
   end
 
   def edit
-    @student = Student.find(params[:id])
   end
 
   def update
-    @student = Student.find(params[:id])
     if @student.update(student_params)
       redirect_to student_path(@student)
     else
@@ -34,12 +33,15 @@ class StudentsController < ApplicationController
   end
 
   def destroy
-    @student = Student.find(params[:id])
     @student.destroy
     redirect_to students_path
   end
 
   private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
 
   def student_params
     params.require(:student).permit(:first_name, :last_name, :email)
