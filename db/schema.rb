fff1550f786c89e46db33a12106fa7c8a6cce009
shaaibu <shaaibusuleiman@gamil.com>
2023-08-24 14:57:52 +0100

    add migration to rename dob attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_24_124011) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_124659) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,7 +28,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_24_124011) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "dob"
-    t.integer "age"
     t.string "address"
     t.string "contact"
   end
