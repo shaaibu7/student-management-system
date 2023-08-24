ec5a7879e4da951e858b0f1779963222e7912eba
shaaibu <shaaibusuleiman@gamil.com>
2023-08-24 13:45:08 +0100

    add attributes to students table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_18_232904) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_124011) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_18_232904) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "dob"
+    t.integer "age"
+    t.string "address"
+    t.string "contact"
   end
 
 end
