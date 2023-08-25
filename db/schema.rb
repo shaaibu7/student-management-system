198bc045e3e1527376334526d1f7045814eba8b1
shaaibu <shaaibusuleiman@gamil.com>
2023-08-25 14:25:03 +0100

    implement reversible migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_24_175908) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_180808) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "demos", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
