571b597161f8e57d5ff2db3aeea43f7e14145036
shaaibu <shaaibusuleiman@gamil.com>
2023-08-29 20:28:12 +0100

    add student-project model for association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_29_183059) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_29_192322) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_29_183059) do
   end
 
   create_table "demos", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", null: false
