a432c249403a9dd00a22404aaa9104d81257e20b
shaaibu <shaaibusuleiman@gamil.com>
2023-08-29 21:48:23 +0100

    setup associations for students and projects

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,8 @@
 class Student < ApplicationRecord
   has_many :blogs
   has_and_belongs_to_many :courses
+  has_many :student_projects
+  has_many :projects, through: :student_projects
   validates :first_name, :last_name, :email, presence: true
 
   before_create :display_greetings
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_29_192322) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_29_192703) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_29_192322) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "student_projects", force: :cascade do |t|
+    t.bigint "student_id", null: false
+    t.bigint "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_student_projects_on_project_id"
+    t.index ["student_id"], name: "index_student_projects_on_student_id"
+  end
+
   create_table "students", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -61,4 +70,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_29_192322) do
     t.string "alternate_contact_number"
   end
 
+  add_foreign_key "student_projects", "projects"
+  add_foreign_key "student_projects", "students"
 end
