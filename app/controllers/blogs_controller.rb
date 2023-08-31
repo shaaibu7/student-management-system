7c7a8eaae080f07c307624e0e28cee82413fa5fa
shaaibu <shaaibusuleiman@gamil.com>
2023-08-31 20:31:13 +0100

    add blog controller

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,47 @@
+class StudentsController < ApplicationController
+  before_action :set_blog, only: %i[show edit update destroy]
+
+  def index
+    @blog = Blog.find_each
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path, notice: 'Blog has been created successfully'
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @blog.update(blog_params)
+      redirect_to blog_path(@blog), notice: 'Blog has been updated successfully'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice: 'Blog has been deleted successfully'
+  end
+
+  private
+
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+end
